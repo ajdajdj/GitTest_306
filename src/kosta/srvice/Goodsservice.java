ed25d4b2b7923dfbf39707d76436742c9aaccc2e
ajdajdj <140366482+ajdajdj@users.noreply.github.com>
2026-02-09 16:42:44 +0900

    Update Goodsservice.java

--- src/kosta/srvice/Goodsservice.java
+++ src/kosta/srvice/Goodsservice.java
@@ -5,6 +5,7 @@ public class Goodsservice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("inset call.");
+		System.out.println("충돌확인해보자");
 		
 
 	}

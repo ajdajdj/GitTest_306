9f1eb7bd0d31ccc708737b2de962b511e726655c
ajdajdj <mrkor007@naver.com>
2026-02-10 12:30:47 +0900

    member테스트

---
+++ src/kosta/controller/member_control.java
@@ -0,0 +1,10 @@
+package kosta.controller;
+
+public class member_control {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("멤버 확인");
+	}
+
+}
